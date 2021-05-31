40ef2c4c9ffec29230a99d258382bb10f25573ee
nichoth <nichoth@gmail.com>
2021-05-30 23:16:13 -0700

    use inline button wrapper

--- src/button.js
+++ src/button.js
@@ -1,9 +1,11 @@
 import { html } from 'htm/preact'
 
 function Button (props) {
-    return html`<div class="form-stuff">
+    return html`<span class="form-stuff">
         ${props.isSpinning ?
-            html`<button ...${props} class=${props.class || '' + ' spinning'}>
+            html`<button ...${props} class=${props.class || '' + ' spinning'}
+                disabled=${true}
+            >
                 <span class="btn-content">${props.children}</span>
             </button>` :
             html`<div >
@@ -12,7 +14,8 @@ function Button (props) {
                 </button>
             </div>`
         }
-        </div>`
+        </span>`
 }
 
 module.exports = Button
+

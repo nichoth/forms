8ffd7f74f708fd35eac33bfc9ba0bdd4c78105d6
nichoth <nichoth@gmail.com>
2021-08-14 21:38:55 -0700

    rm comments

--- src/text-inptut.js
+++ src/text-inptut.js
@@ -1,19 +1,5 @@
 import { html } from 'htm/preact'
 
-// function TextInput (props) {
-//     var { name, displayName } = props
-
-//     return html`<div class="form-stuff">
-//         <div class="input-group ${name}">
-//             <input name="${name}" type="text" placeholder=" "
-//                 required=${props.required} minlength=${props.minlength}
-//                 maxlength=${props.maxlength} id="${name}"
-//             />
-//             <label for=${name}>${displayName}</label>
-//         </div>
-//     </div>`
-// }
-
 function create (html) {
     return function TextInput (props) {
         var { name, displayName } = props
